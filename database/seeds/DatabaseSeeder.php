c20a5579ff83e12e2576e11f171e1cf47d4875ac
saul mamani <luas0_1@yahoo.es>
2020-01-16 07:17:30 -0400

    seeders productos arreglado

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(UsersSeeders::class);
-         $this->call(ProductosSeeders::class);
+         $this->call(ProductosSeeder::class);
     }
 }
